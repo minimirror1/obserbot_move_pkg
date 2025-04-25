f771fe82f3a4227168922b415b3b1386c5c02c51
shs <minimirror1@naver.com>
2025-04-25 01:59:29 +0000

    obserbot_move_head_node.py 파일 추가: 머리 움직임을 제어하는 노드 구현 및 서비스와 토픽 발행 기능 포함. set_sample_5.csv 파일 추가: 새로운 좌표 데이터 제공. obserbot_move_node.py에 test_move_5 함수 추가: CSV 파일에서 좌표 데이터를 읽어 반복 전송하는 기능 구현.

---
+++ scripts/obserbot_move_head_node.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+import rospy
+import time
+from std_msgs.msg import Float64, String
+from std_srvs.srv import Empty, EmptyResponse
+from dynamixel_sdk_examples.msg import SetHeadCommand
+
+class ObserbotMoveHeadNode:
+    def __init__(self):
+        rospy.init_node('obserbot_move_head_node', anonymous=True)
+        
+        # 토픽 발행자 생성
+        self.motor_pub = rospy.Publisher('/canopen/single_motor/j_n/position', Float64, queue_size=10)
+        self.head_cmd_pub = rospy.Publisher('/set_head_command', SetHeadCommand, queue_size=10)
+        
+        # 서비스 초기화
+        rospy.Service('/move_head_up', Empty, self.move_head_up)
+        rospy.Service('/move_head_down', Empty, self.move_head_down)
+        
+        # 토픽 구독자 생성 (필요한 경우)
+        rospy.Subscriber('/move_head', String, self.head_command_callback)
+        
+        rospy.loginfo("ObserbotMoveHeadNode initialized")
+    
+    def move_head_up(self, req=None):
+        """
+        머리를 올리는 함수
+        1. /canopen/single_motor/j_n/position 메시지 송신
+        2. 3초 대기
+        3. /set_head_command 메시지 송신
+        """
+        rospy.loginfo("Moving head up")
+        
+        # 1. j_n 모터 위치 설정 메시지 송신
+        motor_msg = Float64()
+        motor_msg.data = 990000.0
+        self.motor_pub.publish(motor_msg)
+        rospy.loginfo("Published motor position: 990000.0")
+        
+        # 2. 3초 대기
+        rospy.sleep(2.0)
+        
+        # 3. SetHeadCommand 메시지 송신
+        head_cmd = SetHeadCommand()
+        head_cmd.command = 'up'
+        self.head_cmd_pub.publish(head_cmd)
+        rospy.loginfo("Published head command: up")
+        
+        return EmptyResponse()
+    
+    def move_head_down(self, req=None):
+        """
+        머리를 내리는 함수
+        j_n과 SetHeadCommand 메시지를 동시에 송신
+        """
+        rospy.loginfo("Moving head down")
+        
+        # j_n 모터 위치 설정 메시지 송신
+        motor_msg = Float64()
+        motor_msg.data = 5000.0
+        self.motor_pub.publish(motor_msg)
+        rospy.loginfo("Published motor position: 5000.0")
+        
+        # SetHeadCommand 메시지 송신 (동시에)
+        head_cmd = SetHeadCommand()
+        head_cmd.command = 'down'
+        self.head_cmd_pub.publish(head_cmd)
+        rospy.loginfo("Published head command: down")
+        
+        return EmptyResponse()
+    
+    def head_command_callback(self, msg):
+        """
+        문자열 메시지로 머리 움직임 제어
+        """
+        command = msg.data.lower()
+        
+        if command == 'up':
+            self.move_head_up()
+        elif command == 'down':
+            self.move_head_down()
+        else:
+            rospy.logwarn("Unknown head command: %s", command)
+    
+    def run(self):
+        """
+        노드 실행 함수
+        """
+        rospy.loginfo("ObserbotMoveHeadNode is running")
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        # 노드 생성 및 실행
+        node = ObserbotMoveHeadNode()
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
